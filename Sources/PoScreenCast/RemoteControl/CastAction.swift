c95ba49292169d15868735fc243a3218aea06417
HzS <154427919@qq.com>
2024-01-21 12:32:59 +0800

    metadata完善

--- Sources/PoScreenCast/RemoteControl/CastAction.swift
+++ Sources/PoScreenCast/RemoteControl/CastAction.swift
@@ -93,7 +93,7 @@ public struct CastAction {
         switch type {
         case let .setAVTransportURI(uri, videoName):
             xmlEle.addChild(name: "CurrentURI", value: uri)
-            let metadata = "<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\"><item id=\"123\" parentID=\"-1\" restricted=\"1\"><upnp:storageMedium>UNKNOWN</upnp:storageMedium><upnp:writeStatus>UNKNOWN</upnp:writeStatus><dc:title>\(videoName)</dc:title><dc:creator>mgtv</dc:creator><upnp:class>object.item.videoItem</upnp:class><res protocolInfo=\"http-get:*:video/mp4:*;DLNA.ORG_OP=01;DLNA.ORG_FLAGS=01700000000000000000000000000000\">\(uri.xmlEscaped)</res><upnp:class>object.item.videoItem</upnp:class></item></DIDL-Lite>"
+            let metadata = "<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\"><item id=\"123\" parentID=\"-1\" restricted=\"1\"><upnp:storageMedium>UNKNOWN</upnp:storageMedium><upnp:writeStatus>UNKNOWN</upnp:writeStatus><dc:title>\(videoName)</dc:title><dc:creator>mgtv</dc:creator><upnp:class>object.item.videoItem</upnp:class><res protocolInfo=\"http-get:*:video/mp4:*;DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000\">\(uri.xmlEscaped)</res><upnp:class>object.item.videoItem</upnp:class></item></DIDL-Lite>"
             xmlEle.addChild(name: "CurrentURIMetaData", value: metadata)
         case .play:
             xmlEle.addChild(name: "Speed", value: "1")
